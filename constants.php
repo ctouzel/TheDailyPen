47eec0f64efd5a8e7f86c6229a2599256de3518a
Christian Touzel <ctouzel@gmail.com>
2017-04-19 12:16:20 -0400

    Ajout

---
+++ constants.php
@@ -0,0 +1,13 @@
+<?php
+
+define("VERSION", "1.0.4.10009");
+define("TITLE", "The Daily Pen");
+
+define("URL_WEATHER", "http://meteo.gc.ca/rss/city/qc-133_f.xml");
+define("URL_STANDINGS", "http://www.cbssports.com/mlb/standings/regular");
+define("URL_WILDCARD_STANDINGS","http://www.cbssports.com/mlb/standings/wildcard");
+define("URL_SPRING_STANDINGS", "http://www.cbssports.com/mlb/standings/spring");
+
+define("URL_TODOIST", "http://www.todoist.com/API/getProjects?token=a3b4994d3e4525f8e559a8428e2872a90acf4dad");
+define("URL_TODOIST_TASKS", "http://www.todoist.com/API/getUncompletedItems?token=a3b4994d3e4525f8e559a8428e2872a90acf4dad&project_id=");
+?>
